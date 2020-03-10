296b98a0fe7da9a764c935b73ccb6e26f6987fb7
Rodrigo Eltz <rpetrye@hotmail.com>
2020-03-10 12:49:03 -0400

    test

--- Database Location App/Server/server.js
+++ Database Location App/Server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 const cors = require('cors');
 
 
@@ -8,4 +8,10 @@ app.use(cors());
 
 app.listen(8000, () => {
 	console.log('Server started on 8000');
+});
+
+
+app.get('/', (req, res) => {
+	console.log('got request', req);
+	res.send('test');
 });
